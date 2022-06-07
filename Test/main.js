89f847ad4f64cc6960eab0e66fb82d9bf423d37b
Onishchuk-bit <81750516+Onishchuk-bit@users.noreply.github.com>
2022-06-07 21:47:28 +0300

    Add files via upload

---
+++ Test/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    $('.dropdown-item').hide();
+    $('.active').show();
+
+    $('.active').click(function(){
+        $('.dropdown-item').show();
+    });
+    $('.active')
+        
+});
+
+    // let flag = false;
+    // $('.active').click(function(){
+    //     if(!flag) {
+    //         $('.dropdown-item').slideDown()
+    //     } else {
+    //         $('.dropdown-item')slideup()
+    //     }
+    // })
+
+    
+
